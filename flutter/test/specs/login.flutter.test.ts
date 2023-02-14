3cab05619012f61c844af38e695cca5df917dfbc
Ashawami <ashawami.mulik@thoughtworks.com>
2023-02-14 18:31:13 +0530

    [Appium-Flutter-Driver] Remove unnecessary code from Login test

--- flutter/test/specs/login.flutter.test.ts
+++ flutter/test/specs/login.flutter.test.ts
@@ -1,18 +1,15 @@
 import LoginFlutter from '../pages/login';
 import CatalogFlutter from '../pages/catalog';
-import Catalog from '../../../test/pages/catalog';
 import { testData } from '../test_data/testdata.flutter';
 import { assert } from 'chai';
 
 describe('Login', () => {
   let login: LoginFlutter;
   let catalog: CatalogFlutter;
-  let c: Catalog;
 
   beforeEach(() => {
     login = new LoginFlutter();
     catalog = new CatalogFlutter();
-    c = new Catalog();
   });
 
   it('should login with valid credentials', async () => {
