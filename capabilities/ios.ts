7f769103f5877b809340c6f1e60f5cec62bc1a12
Ashawami <ashawami.mulik@thoughtworks.com>
2023-02-16 13:37:59 +0530

    [Appium] Add IOS config

---
+++ capabilities/ios.ts
@@ -0,0 +1,20 @@
+export const capabilities = [
+  {
+    // deviceName: 'Samsung Galaxy S.*',
+    'appium:deviceName': 'iPhone 14',
+    'appium:udid': '0A5E9FF0-44DE-4E7F-B701-12623AA244C1',
+    // deviceOrientation: 'portrait',
+    'appium:platformVersion': '16.1',
+    platformName: 'iOS',
+    'appium:app':
+      '/Users/ashawami/Library/Developer/Xcode/DerivedData/Runner-bdfhbizpffvrpffbjtnnlmqgdthw/Build/Products/Debug-iphonesimulator/Runner.app',
+    // appPackage: 'com.aral.aral',
+    'appium:autoGrantPermissions': true,
+    // appActivity: '.MainActivity',
+    // appActivity: '.SplashActivity',
+    // commandTimeout: 200,
+    'appium:automationName': 'XCUITest',
+    // appWaitActivity: '.MainActivity,.SplashActivity',
+    // appWaitDuration: 60000,
+  },
+];
