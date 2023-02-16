c3cd7792418a60a7f47ce1df3ee63e3add005654
Ashawami <ashawami.mulik@thoughtworks.com>
2023-02-16 11:40:28 +0530

    [Appium-Flutter-driver] Add Scroll functions

--- flutter/test/pages/catalog.ts
+++ flutter/test/pages/catalog.ts
@@ -1,4 +1,4 @@
-import { byText } from 'appium-flutter-finder';
+import { byText, byType } from 'appium-flutter-finder';
 
 class CatalogFlutter {
   public pageTitleText = 'Catalog';
@@ -10,6 +10,23 @@ class CatalogFlutter {
   public async getPageTitle() {
     const text = await driver.getElementText(this.pageTitle);
     return text;
+  }
+
+  public async scroll(dx: number, dy: number, duration: number, frequency: number) {
+    await driver.execute('flutter:scroll', byType('Scrollable'), {
+      dx: dx,
+      dy: dy,
+      durationMilliseconds: duration,
+      frequency: frequency,
+    });
+  }
+
+  public async scrollUntilVisible(item: string, dx: number, dy, number) {
+    await driver.execute('flutter:scrollUntilVisible', byType('Scrollable'), {
+      item: byText(`${item}`),
+      dxScroll: dx,
+      dyScroll: dy,
+    });
   }
 }
 
